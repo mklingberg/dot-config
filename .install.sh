347560f343baced3ed79761c4f3a7fce12dc3584
Marcus Klingberg <marcus.klingberg@gmail.com>
2025-09-03 17:12:08 +0200

    Replace arc with zen in Brew Cask installation list

--- .install.sh
+++ .install.sh
@@ -51,7 +51,7 @@ brew install --cask slack
 brew install --cask microsoft-teams
 
 ### Web
-brew install --cask arc
+brew install --cask zen
 brew install --cask google-chrome
 brew install --cask firefox
 
