428e7b54347887af775c4b052648124d9c4ed5ca
Marcus Klingberg <marcus.klingberg@gmail.com>
2026-06-06 21:25:39 +0200

    Add configuration files for sketchybar and wezterm, including themes and launch scripts to _archive folder

---
+++ _archive/.start_on_login.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Launch sketchybar
+/opt/homebrew/bin/sketchybar &
+
+# Launch sketchybar-2
+/opt/homebrew/bin/sketchybar-2
+
+# Launch sketchybar-3
+#/opt/homebrew/bin/sketchybar-3 &
